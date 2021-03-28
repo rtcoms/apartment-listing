a5484b1ed725e69b6632663cadf595aa07f4441d
Rohit Trivedi <86.rohit@gmail.com>
2021-03-28 10:34:00 +0530

    fix apartment routes

--- spec/requests/api/v1/apartments_spec.rb
+++ spec/requests/api/v1/apartments_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 require 'json_expressions/rspec'
 
 RSpec.describe "Api::V1::Apartments", type: :request do
-  describe "GET /index" do
+  describe "GET index" do
     let!(:apartment_villa) { create(:apartment, title: 'Villa', price: 500000, area: 5000, number_of_bedrooms: 5, number_of_bathrooms: 3) }
     let!(:apartment_flat_2bhk) { create(:apartment, title: 'Flat 2bhk', price: 50000, area: 500, number_of_bedrooms: 2, number_of_bathrooms: 1) }
     let!(:apartment_flat_3bhk) { create(:apartment, title: 'Flat 3bhk', price: 150000, area: 1500, number_of_bedrooms: 3, number_of_bathrooms: 2) }
     let!(:apartment_studio) { create(:apartment, title: 'Flat Studio', price: 100000, area: 1000, number_of_bedrooms: 1, number_of_bathrooms: 1) }
 
     it "returns http success" do
-      get "/api/v1/apartments/index"
+      get "/api/v1/apartments"
 
       expected_response = [
         { id: apartment_villa.id, title: apartment_villa.title }.ignore_extra_keys!,
@@ -23,7 +23,7 @@ RSpec.describe "Api::V1::Apartments", type: :request do
     end
 
     it "returns http success" do
-      get "/api/v1/apartments/index", params: {query: { number_of_bedrooms_gte: 2, number_of_bathrooms_gte: 2, area_gte: 1500, price_gte: 500000 }}
+      get "/api/v1/apartments", params: {query: { number_of_bedrooms_gte: 2, number_of_bathrooms_gte: 2, area_gte: 1500, price_gte: 500000 }}
 
       expected_response = [
         { id: apartment_villa.id, title: apartment_villa.title }.ignore_extra_keys!,
