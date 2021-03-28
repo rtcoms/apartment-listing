1c357670175d686baa06d82c489573c04fb04549
Rohit Trivedi <86.rohit@gmail.com>
2021-03-28 13:25:13 +0530

    check if page filter working or not

--- app/javascript/packs/components/Apartments.jsx
+++ app/javascript/packs/components/Apartments.jsx
@@ -5,7 +5,7 @@ import ApartmentList from './ApartmentList.jsx';
 const Apartments = props => (
   <React.Fragment>
     <div>Apartments Listing</div>
-    <ApartmentList searchParams={{query: {price_gte: 0, price_lte: 100000}}} />
+    <ApartmentList searchParams={{page: 2, query: {price_gte: 0, price_lte: 100000}}} />
     <Link to={`/`}>
       Back to Home
     </Link>
