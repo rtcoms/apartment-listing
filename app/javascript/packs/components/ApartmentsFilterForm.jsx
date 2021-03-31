478950972f430089c6915581e3b4e873c4ceb56c
Rohit Trivedi <86.rohit@gmail.com>
2021-04-01 01:23:23 +0530

    use react-hook-form instead of formik

--- app/javascript/packs/components/ApartmentsFilterForm.jsx
+++ app/javascript/packs/components/ApartmentsFilterForm.jsx
@@ -4,40 +4,72 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useHistory } from "react-router-dom";
 import { useStoreActions } from 'easy-peasy';
 import { useLocation } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { Box, Center, Text, Stack, HStack, VStack, Input, Radio, RadioGroup,  NumberInput, SimpleGrid, Button } from "@chakra-ui/react"
+import './form.scss';
 
  const ApartmentsFilterForm = ({redirect}) =>  {
   let location = useLocation();
   const history = useHistory();
+  const { register, handleSubmit, watch, errors } = useForm();
   const updateQueryFilters = useStoreActions((actions) => actions.apartmentFilters.updateQueryFilters);
+
+  const onSubmit = (data) => {
+    console.log('------filter form submission');
+    console.log(data);
+
+    const queryString = qs.stringify({query: data})
+    history.push(`/apartments?${queryString}`);
+  };
 
    return <div>
      <h1>Search Apartments</h1>
-     <Formik
-       initialValues={{ price_gte: '', price_lte: '', area_gte: '', area_lte: ''  }}
-       onSubmit={(values, { setSubmitting }) => {
-         setTimeout(() => {
-           if(redirect) {
-            const queryString = qs.stringify({query: values})
-            history.push(`/apartments?${queryString}`);
-           } else {
-            updateQueryFilters(values);
-           }
-           setSubmitting(false);
-         }, 400);
-       }}
-     >
-       {({ isSubmitting }) => (
-         <Form>
-           <Field type="number" name="price_gte" />
-           <Field type="number" name="price_lte" />
-           <Field type="number" name="area_gte" />
-           <Field type="number" name="area_lte" />
-           <button type="submit" disabled={isSubmitting}>
-             Submit
-           </button>
-         </Form>
-       )}
-     </Formik>
+     <form onSubmit={handleSubmit(onSubmit)}>
+        <VStack color='black'>
+          <Center>
+            <VStack>
+              <Text>Price</Text>
+              <SimpleGrid columns={{sm: 2, md: 2}} spacing="40px">
+                <Input borderWidth="1px" type='number' name="price_gte" placeholder="Price(min)" defaultValue="" ref={register} />
+                <Input borderWidth="1px" type='number' name="price_lte" placeholder="Price(max)" defaultValue="" ref={register} />
+              </SimpleGrid>
+            </VStack>
+          </Center>
+
+          <Center>
+            <VStack>
+              <Text>Area</Text>
+              <SimpleGrid columns={{sm: 2, md: 2}} spacing="40px">
+                <Input borderWidth="1px" type='number' name="area_gte" placeholder="Area(min)" defaultValue="" ref={register} />
+                <Input borderWidth="1px" type='number' name="area_lte" placeholder="Area(max)" defaultValue="" ref={register} />
+              </SimpleGrid>
+            </VStack>
+          </Center>
+
+          <Center>
+            <VStack>
+              <Text>Number of Bedrooms</Text>
+              <div classname="radioGroup">
+                <RadioGroup name='number_of_bedrooms_gte'>
+                  <HStack direction="row">
+                    <label className='radio-btn'><Radio value='1' ref={register} className={'radio'} /> 1+</label>
+                    <label className='radio-btn'><Radio value='2' ref={register} className={'radio'} /> 2+</label>
+                    <label className='radio-btn'><Radio value='3' ref={register} className={'radio'} /> 3+</label>
+                    <label className='radio-btn'><Radio value='4' ref={register} className={'radio'} /> 4+</label>
+                    <label className='radio-btn'><Radio value='5' ref={register} className={'radio'} /> 5+</label>
+                  </HStack>
+                </RadioGroup>
+
+                <span class="indicator"></span>
+              </div>
+            </VStack>
+          </Center>
+        </VStack>
+
+        <Center>
+          <Button mt={4} type="submit">Submit</Button>
+        </Center>
+      </form>
    </div>
  };
 
