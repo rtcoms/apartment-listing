bec3593e179b255d1e5f86123cc79632a854a801
Rohit Trivedi <86.rohit@gmail.com>
2021-04-01 19:08:27 +0530

    homepage style fixed

--- app/javascript/packs/components/Home.jsx
+++ app/javascript/packs/components/Home.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from "react-router-dom";
-import ApartmentsFilterForm from './ApartmentsFilterForm.jsx';
+import { Button, Flex, Heading, Stack, Text, useBreakpointValue, } from '@chakra-ui/react';
 
-const Home = props => (
-  <React.Fragment>
-    <div>Home {props.name}!</div>
-    <Link to={`/apartments`}>View Apartments</Link>
-  </React.Fragment>
+const Home = () => (
+  <Stack minH={'100vh'}>
+      <Flex p={8} flex={1} align={'center'} justify={'center'}>
+        <Stack>
+          <Heading>
+            <Text as={'span'} position={'relative'}>Hello Kasaz</Text>
+            <br />
+            <Text color={'blue.400'} as={'span'}>Aparmtnet Listing</Text>
+          </Heading>
+          <Text fontSize='md' color={'gray.500'}>This is an apartment listing management project</Text>
+          <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+            <Button rounded={'full'} bg={'blue.400'} color={'white'} >
+              <Link to={`/apartments`}>View Apartments</Link>
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+    </Stack>
 )
 
 Home.defaultProps = {
